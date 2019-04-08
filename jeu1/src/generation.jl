a496761d5a2521b3da4b95c73cbf2a606359ebf5
Guilmeau <thomas.guilmeau@ensta-paristech.fr>
2019-04-08 11:55:03 +0200

    ajout de la fonction generateInstance avec des probas

--- jeu1/src/generation.jl
+++ jeu1/src/generation.jl
@@ -2,16 +2,40 @@
 include("io.jl")
 
 """
-Generate an n*n grid
+Let n be the maximal number reached on the domino tiles.
+Beware that 0 is also on the domino tiles.
 
-Argument
-- n: size of the grid
+Generate an (n+1)*(n+2) grid
 """
+
+
 function generateInstance(n::Int64)
-    ## grid
+"""
+this function generates an array which corresponds to the grid to be paved
+"""
 
-
-    # TODO
+	"cette grille représente la grille à paver"	
+	grille = Array{Int64}(undef, n+1, n+2)
+	"remplissage[i] = nbr de fois que le chiffre i a été placé dans la grille"
+	remplissage = zeros(n+1)
+	"probabilité de placer i à l'instant t"
+	proba = ones(n+1)*(1/(n+1))
+	
+	for nl in 1:(n+1)
+		for nc in 1:(n+2)
+			p = rand()
+			i = 0
+			seuil=proba[0]
+			while p>seuil
+				i = i+1
+				seuil = seuil+proba[i]
+			end	
+			grille[nl][nc] = i
+			proba[i] = proba[i] - 1/((n+1)*(n+2))
+			proba= proba + 1/((n+1)*(n+1)*(n+2))
+		end
+	end
+	return grille
     println("In file generation.jl, in method generateInstance(), TODO: generate an instance")
     
 end 
@@ -21,7 +45,14 @@ Generate all the instances
 
 Remark: a grid is generated only if the corresponding output file does not already exist
 """
-function generateDataSet()
+
+
+
+function generateDataSet(Array{Int64})
+"""
+This function takes as an argument the array previously generated.
+This function returns a .txt file containing all the informations
+"""
 
     # TODO
     println("In file generation.jl, in method generateDataSet(), TODO: generate an instance")
