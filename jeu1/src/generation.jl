03aa5cb27ac69ea9bf7ca7326e3493ad938c8f91
Guilmeau <thomas.guilmeau@ensta-paristech.fr>
2019-04-15 12:17:04 +0200

    réalisé la fonction generateDataSet

--- jeu1/src/generation.jl
+++ jeu1/src/generation.jl
@@ -24,23 +24,25 @@ this function generates an array which corresponds to the grid to be paved
 
 	for nl in 1:(n+1)
 		for nc in 1:(n+2)
-			"mise à jour duvecteur des probabilités"
+			"mise à jour du vecteur des probabilités"
 			for k in 1:(n+1)
 				proba[k] = ((n+2)-n_t[k])/((n+1)*(n+2)-t)
 			end
 
 			p = rand()
-			i = 1
+			i = Int(1)
 			seuil=proba[1]
 			while p>seuil			
 				seuil = seuil+proba[i+1]
 				i = i+1
 			end	
-			grille[nl,nc] = i-1
+			grille[nl,nc] = Int64(i-1)
 			n_t[i] = n_t[i] + 1
-			t = t + 1			
+			t = t + 1
+			println(typeof(i))			
 		end
 	end
+	
 	return grille
     println("In file generation.jl, in method generateInstance(), TODO: generate an instance") 
 end 
@@ -54,7 +56,7 @@ Remark: a grid is generated only if the corresponding output file does not alrea
 
 
 
-function generateDataSet(grid::Array{Int64})
+function generateDataSet(grid::Array{Int64,2})
 """
 This function takes as an argument the array previously generated.
 This function returns a .txt file containing all the informations
@@ -64,7 +66,7 @@ This function returns a .txt file containing all the informations
 	nc = size(grid, 2)
 	for i in 1:nl
 		for j in 1:nc
-			res = res*string(grid[nl,nc])
+			res = res*string(grid[i,j])
 			if j<nc
 				res = res*" "
 			else
@@ -72,15 +74,17 @@ This function returns a .txt file containing all the informations
 			end
 		end
 	end
+	touch("output.txt")
 	myFile = open("output.txt", "w")
 	# Ecrire "test" dans ce fichier
-	println(fout, "res")
-	close(fout)
+	println(myFile, res)
+	close(myFile)
     # TODO
     println("In file generation.jl, in method generateDataSet(), TODO: generate an instance")
     
 end
 
+generateDataSet(generateInstance(4))
 
 
 
